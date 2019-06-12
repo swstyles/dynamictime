6c9ef6cfedf4ed09c4e5d08c7b306fed3110c130
Daniel Magallanes <50222859+swstyles@users.noreply.github.com>
2019-06-11 20:35:37 -0700

    initial upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// DOM Elements
+const time = document.getElementById("time")
+    ,greeting = document.getElementById("greeting")
+    ,name = document.getElementById("name")
+    ,focus = document.getElementById("focus");
+
+// Show Time
+function showTime() {
+    let today = new Date()
+        ,hour = today.getHours()
+        ,min = today.getMinutes()
+        ,sec = today.getSeconds();
+
+    // Set AM or PM
+    const amPm = hour >= 12 ? 'PM' : 'AM';
+
+    //12 Hour Format
+    hour = hour % 12 || 12;
+
+    //Output Time
+    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>:${sec}`;
+
+    setTimeout(showTime, 1000);
+}
+
+// Run
+showTime();
